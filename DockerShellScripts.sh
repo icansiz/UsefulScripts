67eecd4ae3dfe36a22274614783f78ed896c139c
icansiz <34852195+icansiz@users.noreply.github.com>
2024-06-05 15:08:50 +0300

    Update DockerShellScripts.sh

--- DockerShellScripts.sh
+++ DockerShellScripts.sh
@@ -1,2 +1,8 @@
 // Docker container filtering and get ids
 docker ps -a|grep Exited|awk '{print $1}'
+// or
+docker ps -a|grep Exited|cut -f 1 -d " "
+
+// Remove all stopped containers
+docker container prune
+
